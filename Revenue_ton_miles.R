5fb82164985c80ca33c1739e7f25fe52387eed03
nadoune <nada.habli@canada.ca>
2023-01-25 21:35:35 -0500

    Update Revenue_ton_miles.R

--- Revenue_ton_miles.R
+++ Revenue_ton_miles.R
@@ -1,4 +1,4 @@
-destfile <- "Data/Supply_Chain.xlsx"
+destfile <- "Data/Revenue_ton_miles.xlsx"
 
 url = "https://www.cn.ca/-/media/Files/Investors/Investor-Performance-Measures/CN-BU-WEBSUMMARYpv.xlsx"
 download.file(url, destfile, mode = "wb")
