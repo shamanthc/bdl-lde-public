f8c5991f66197f795591a2c583167850b52d45d3
nadoune <nada.habli@canada.ca>
2023-01-27 10:20:39 -0500

    Update Google_Trend.R

--- Google_Trend.R
+++ Google_Trend.R
@@ -1,14 +1,14 @@
 library(gtrendsR)
 
-recession <- gtrendsR::gtrends("Recession", onlyInterest = TRUE, geo = "CA")
+recession <- gtrendsR::gtrends("Recession", onlyInterest = TRUE, geo = "CA", time = "all")
 recession1 <-data.frame(recession$interest_over_time)
 
 
-inflation <- gtrendsR::gtrends("Inflation", onlyInterest = TRUE, geo = "CA")
+inflation <- gtrendsR::gtrends("Inflation", onlyInterest = TRUE, geo = "CA", time="all")
 inflation1 <-data.frame(inflation$interest_over_time)
 
 
-supplyChain <- gtrendsR::gtrends("Supply chain", onlyInterest = TRUE, geo = "CA")
+supplyChain <- gtrendsR::gtrends("Supply chain", onlyInterest = TRUE, geo = "CA", time="all")
 supplyChain1 <-data.frame(supplyChain$interest_over_time)
 
 write.csv(recession1, file   = paste0("Data/Recession_trends.csv")  , row.names = F)
