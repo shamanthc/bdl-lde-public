fb4a7f0e575968e8c09d134949e5a1c398a5e08c
Dakshdeep Singh <77427849+IamDakshdeep@users.noreply.github.com>
2023-02-09 13:43:16 -0500

    Update Revenue_ton_miles.R

--- Revenue_ton_miles.R
+++ Revenue_ton_miles.R
@@ -1,7 +1,7 @@
-destfile <- "Data/Revenue_ton_miles.xlsx"
-
-url = "https://www.cn.ca/-/media/Files/Investors/Investor-Performance-Measures/CN-BU-WEBSUMMARYpv.xlsx"
-download.file(url, destfile, mode = "wb")
+#destfile <- "Data/Revenue_ton_miles.xlsx"
+#
+#url = "https://www.cn.ca/-/media/Files/Investors/Investor-Performance-Measures/CN-BU-WEBSUMMARYpv.xlsx"
+#download.file(url, destfile, mode = "wb")
 
 library(readxl)
 library(dplyr)
