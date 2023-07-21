6cf56ed78a4b49bed8fc28486c49602817d3606d
Dakshdeep Singh <77427849+IamDakshdeep@users.noreply.github.com>
2023-07-21 11:41:02 -0400

    Update Revenue_ton_miles.R

--- Revenue_ton_miles.R
+++ Revenue_ton_miles.R
@@ -9,6 +9,7 @@ library(janitor)
 
 destfile <- "Data/Revenue_ton_miles_V2.xlsx"
 
+
 url = "https://www.cn.ca/-/media/Files/Investors/Investor-Performance-Measures/CN-BU-WEBSUMMARYpv.xlsx"
 
 download.file(url, destfile, mode = "wb")
